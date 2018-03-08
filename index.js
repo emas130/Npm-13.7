704b8c316a7dd8805db7305f134199526423202a
emas130 <34860891+emas130@users.noreply.github.com>
2018-03-08 22:58:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var colors = require('colors');
 
 fs.readdir('../18 Node-npm','utf-8', function (err, files) {
 	console.log('Pobieram dane'.green);
-
+	console.log(files);
 	fs.writeFile('./tekst.txt', ' Wszystko chodzi jak należy!', function (err) {
 		if (err) throw err;
 		console.log('Zapisano!'.gray);
